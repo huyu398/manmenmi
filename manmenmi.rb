fbb85bcbcb028d52ae5130471ca2a916162d94ce
huyu398 <huyu.sakuya4645@gmail.com>
2016-03-03 19:12:16 +0900

    サイボウズ Office 10 からは javaScript が必要なので， capybara に乗り換える

--- manmenmi.rb
+++ manmenmi.rb
@@ -1,4 +1,5 @@
-require 'mechanize'
+require 'capybara'
+require 'capybara-webkit'
 require 'logger'
 require 'yaml'
 
@@ -37,33 +38,35 @@ loop do
     raise Errors::UserRecognizeError.new("No user with uid:#{tag}", __LINE__) if user.empty?
     logger.info("Recognize a user with id:#{user['id']} name:#{user['name']}")
 
-    agent = Mechanize.new
+    session = Capybara::Session.new(:webkit)
 
     # login
-    login_form = agent.get(URL).form
-    login_form._ID = user['id']
-    login_form.Password = user['password']
-    user_page = agent.submit(login_form)
-    raise Errors::LoginError.new("Invalid password for user:#{user['name']}", __LINE__) if user_page.title.match(/エラー/)
+    session.visit(URL)
+    session.select(user['name'], from: '_ID')
+    session.fill_in('Password', with: user['password'])
+    session.click_button('ログイン')
+    raise Errors::LoginError.new("Invalid password for user:#{user['name']}", __LINE__) if session.title.match(/エラー/)
     logger.info('Success login')
 
     # arrived or left
-    timecard_form = user_page.forms_with(method: 'POST')[2]
-    arrive_timetable = (user_page / 'html/body/div/div/div/table/tr/td/table/tr/#cb7-portal-left/#cb7-portlet-frame-41/#cb7-portlet-41/#cb7-portlet-body-41/form/table/tr/td[1]')[0]
-    arrive_time = Time.parse(arrive_timetable.children[2].text)
     login_time = Time.now
+    timecard_table = session.all(:xpath, 'html/body/div/div/div/table/tbody/tr/td/table/tbody/tr/td/div/div/div/form/table/tbody/tr/td')
+    arrive_cell, leave_cell = [timecard_table.at(0), timecard_table.at(1)]
+    arrive_time = Time.parse(arrive_cell.text.match(/\d{2}:\d{2}/)[0])
 
     if login_time < arrive_time + 15 * 60 # margin 15min
       `mplayer #{AUDIO_PREFIX}/goodmorn.wav 2>/dev/null`
       logger.info("#{user['name']} already logined cybozu")
     else
-      if timecard_form.button_with(value: '出社')
+      if arrive_cell.has_button?('出社')
+        leave_cell.click_button('出社')
+        raise Errors::UnexpectedError.new("!!! Unexpeced error !!!", __LINE__) if session.title.match(/エラー/)
         `mplayer #{AUDIO_PREFIX}/goodmorn.wav 2>/dev/null`
-        timecard_form.click_button
         logger.info("#{user['name']} arrived office")
-      elsif timecard_form.button_with(value: '退社')
+      elsif leave_cell.has_button?('退社')
+        leave_cell.click_button('退社')
+        raise Errors::UnexpectedError.new("!!! Unexpeced error !!!", __LINE__) if session.title.match(/エラー/)
         `mplayer #{AUDIO_PREFIX}/goodbay.wav 2>/dev/null`
-        timecard_form.click_button
         logger.info("#{user['name']} left office")
       else
         raise Errors::UnexpectedError.new("!!! Unexpeced error !!!", __LINE__)
